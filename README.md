41ccf36351ac88e8fbbf5067b4615059e3b5bc71
Tony Sandro <85088970+TonySandro@users.noreply.github.com>
2022-08-09 23:18:57 -0300

    feat: add jest configs

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # speedtest-ticonnect
  
+Applyed rules to Conventional Commits
+https://www.conventionalcommits.org/en/v1.0.0/
---
+++ jest.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  roots: ["<rootDir>/src"],
+  // clearMocks: true,
+  // collectCoverage: true,
+  collectCoverageFrom: [
+    "<rootDir>/src/**/*.ts",
+    "!<rootDir>/src/main/**",
+    "!<rootDir>/src/**/*-protocols.ts",
+    "!<rootDir>/src/**/index.ts",
+  ],
+  coverageDirectory: "coverage",
+  // coverageProvider: "v8",
+  testEnvironment: "node",
+  preset: "@shelf/jest-mongodb",
+  transform: {
+    ".+\\.ts$": "ts-jest",
+  },
+};
