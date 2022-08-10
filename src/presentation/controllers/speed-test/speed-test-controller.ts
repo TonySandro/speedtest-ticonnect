418ab32501f1b10a52b1d06894eadfa1695d842e
Tony Sandro <85088970+TonySandro@users.noreply.github.com>
2022-08-09 23:30:00 -0300

    feat: add SpeedTestController and return 200 if valid data is provided

---
+++ src/presentation/controllers/speed-test/speed-test-controller.ts
@@ -0,0 +1,24 @@
+// import { UniversalSpeedtest, SpeedUnits } from "universal-speedtest";
+
+import { success } from "../../helpers/http/http-helper";
+import { HttpResponse } from "../../protocols/http";
+
+// const universalSpeedtest = new UniversalSpeedtest({
+//     measureUpload: true,
+//     // uploadUnit: SpeedUnits.MBps,
+//     downloadUnit: SpeedUnits.MBps
+// });
+
+// universalSpeedtest.runSpeedtestNet().then(result => {
+//     console.log(`Ping: ${result.ping} ms`);
+//     console.log(`Download speed: ${result.downloadSpeed} MBps`);
+//     console.log(`Upload speed: ${result.uploadSpeed} Mbps`);
+// }).catch(e => {
+//     console.error(e.message);
+// });
+
+export class SpeedTestController {
+    async handle(): Promise<HttpResponse> {
+        return success(null)
+    }
+}
