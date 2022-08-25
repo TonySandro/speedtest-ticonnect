120dd033cd0231c49835ed1fe45939ebd5de350f
Tony Sandro <85088970+TonySandro@users.noreply.github.com>
2022-08-24 21:58:31 -0300

    feat: add fets front

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,26 @@
-import logo from './assets/logoTI.png';
-import './App.css';
+import logo from "./assets/logoTI.png";
+import axios from "axios";
+import "./App.css";
+import ApiHost from "./service/api";
+
+async function speedTest() {
+  console.log("run");
+  const response = await axios.get(`${ApiHost}/api/speed-test`).then((res) => {
+    return res.data;
+  });
+
+  return response;
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <a class="round-button" > INICIAR </a>
+        {/* <button>
+        </button> */}
+        <a className="round-button" type="button" onClick={speedTest}>
+          INICIAR
+        </a>
         <img src={logo} className="App-logo" alt="logo" />
       </header>
     </div>
