fa25fb0393c813e3f0334222805f310f0bbf77de
Tony Sandro <85088970+TonySandro@users.noreply.github.com>
2022-08-25 21:10:38 -0300

    refactor: adjust speed units

--- backend/src/presentation/helpers/services/runSpeedtestNet.ts
+++ backend/src/presentation/helpers/services/runSpeedtestNet.ts
@@ -1,17 +1,14 @@
 import { ResponseSpeedTest } from './../../../domain/models/responseSpeedTest';
-import { UniversalSpeedtest, SpeedUnits } from "universal-speedtest";
+import { UniversalSpeedtest } from "universal-speedtest";
 
 export class RunSpeedtestNet {
     async run(): Promise<ResponseSpeedTest> {
         const universalSpeedtest = new UniversalSpeedtest({
-            measureUpload: true,
-            downloadUnit: SpeedUnits.MBps
+            measureUpload: true
+            // downloadUnit: SpeedUnits.MBps
         });
 
         return universalSpeedtest.runSpeedtestNet().then(result => {
-            console.log(`Ping: ${result.ping} ms`);
-            console.log(`Download speed: ${result.downloadSpeed} MBps`);
-            console.log(`Upload speed: ${result.uploadSpeed} Mbps`);
             return result
         }).catch(e => {
             console.error(e.message);
